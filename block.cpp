1b79b71f46da86fb5335e547ccb025c8effdd554
SboneloMdluli <1101772@students.wits.ac.za>
2019-03-17 14:23:54 +0200

    threaded block

--- block.cpp
+++ block.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <cstdlib>
-//#include <pthreads.h>
+#include <pthread.h>
 #include <vector>
 
 using namespace std;
 
 using tensor = vector <vector<long int>>;
 
-
 int block_len=2;
 long int size_mat =8;
 tensor _MAT;
+const int NUM_THREADS=2;
+int next_pos=NUM_THREADS;
+pthread_mutex_t lock;
+int posY=0;
 
 void CreateMatrix(){
     long int val=1;
@@ -73,14 +76,29 @@ void transpose(tensor& arr){        // normal transpose algorithm
     }
 }
 
-void transpose_blocks(){        // transposing inside of all blocks in matrix
-      for (int i=0;i<size_mat;i+=block_len){
-        for (int j=0;j<size_mat;j+=block_len){
-            tensor block = getSubMatrix(i,j);
-            transpose(block);
-            setSubMatrix(i,j,block);
-        }
-      }    
+void* transpose_blocks(void* ind_){        // transposing inside of all blocks in matrix
+    
+    long local;
+    local = (long) ind_;
+
+    int pos;
+    tensor block;
+
+   while (1){
+    pos = (block_len*local)%size_mat;
+    if ((pos==0) && (local!=0)) {posY+=block_len;} 
+    if ((posY!=0) && (posY%size_mat==0)){break;}
+    block = getSubMatrix(posY,pos);
+    transpose(block);
+    setSubMatrix(posY,pos,block);
+
+    pthread_mutex_lock(&lock);
+    local= next_pos;
+    next_pos++; 
+	pthread_mutex_unlock(&lock);
+    
+    }
+
 }
 
 void shuffle(){             // transposing actual blocks
@@ -94,18 +112,26 @@ void shuffle(){             // transposing actual blocks
       }
 }
 
-
-
-
-
-
 int main(){
     
     CreateMatrix();
-    PrintMatrix();
-    cout <<endl;
-    transpose_blocks();
-    shuffle();
+    pthread_t threads[NUM_THREADS];
+    int index[NUM_THREADS];
+
+    for (int i=0; i<NUM_THREADS;i++){
+        index[i]=i;
+        pthread_create(&threads[i],NULL,transpose_blocks,(void*)index[i]);
+    }
+    
+    for (int i=0; i<NUM_THREADS;i++){
+        pthread_join(threads[i],NULL);
+    }
+
+        //for (int i=0; i<NUM_THREADS;i++){
+   //     index[i]=i;
+    //    pthread_create(&threads[i],NULL,transpose_blocks,(void*)index[i]);
+   // }
+    //shuffle();
     PrintMatrix();
     
     return 0;
